621122d30eff99fbe41926994177e4f4a9bc372d
leesungeon <79202877+leesungeon@users.noreply.github.com>
2021-05-07 16:46:15 +0900

    Fixing

--- BaseballAR/Assets/Eon/1. Scripts/HitingBall.cs
+++ BaseballAR/Assets/Eon/1. Scripts/HitingBall.cs
@@ -22,6 +22,7 @@ public class HitingBall : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.Slerp(transform.position, fallZone, 0.02f);
+        print(gameObject.transform.position.y);
         Destroy(gameObject, DestroySec);
     }
 
